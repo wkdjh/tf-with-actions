1e740de1f8cc8462f2c0d2d695213e1797ca2462
wkdjh <wkdjh1019@gmail.com>
2025-06-25 13:15:09 +0900

    add vpc

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "backend-vpc" {
     Name = "tf-backend2"
   }
 }
+
+resource "aws_vpc" "tf-test-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tf-test-vpc"
+  }
+}
