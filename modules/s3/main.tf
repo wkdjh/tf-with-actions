75bf932963a83a6923ce6a93fd6dfe586beddaac
wkdjh <wkdjh1019@gmail.com>
2025-06-25 13:12:11 +0900

    fix *.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "backend-bucket" {
 
 # remote state 버전 관리
 resource "aws_s3_bucket_versioning" "backend-bucket-versioning" {
-  bucket = aws_s3_bucket.backend-bucket.id 
+  bucket = aws_s3_bucket.backend-bucket.id
 
   versioning_configuration {
     status = "Enabled"
